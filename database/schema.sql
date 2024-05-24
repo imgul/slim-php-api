5819559f533aa996142e6723cadce28c79ac9117
Gul Zaib <bc210411296mgu@vu.edu.pk>
2024-05-24 14:07:47 -0700

    :sparkles: Feat(DB): Copy shema from console to sql file

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,125 @@
-#
+-- DB Schema
+
+CREATE DATABASE IF NOT EXISTS easecloud_api;
+
+CREATE USER 'easecloud_api'@'localhost' IDENTIFIED BY 'easecloud_api';
+GRANT ALL PRIVILEGES ON *.* TO 'easecloud_api'@'localhost';
+FLUSH PRIVILEGES;
+USE easecloud_api;
+
+
+-- Create tables
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+
+    role VARCHAR(255) NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_by INT NULL,
+    deleted_at DATETIME NULL,
+    deleted_by INT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (created_by) REFERENCES users(id),
+    FOREIGN KEY (deleted_by) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS contact_forms (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    action VARCHAR(255) NOT NULL UNIQUE,
+    host VARCHAR(255) NOT NULL,
+    generated_by INT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at DATETIME NULL,
+    deleted_by INT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (generated_by) REFERENCES users(id),
+    FOREIGN KEY (deleted_by) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS cf_labels (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    color VARCHAR(255) NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at DATETIME NULL,
+    deleted_by INT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (deleted_by) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS cf_inbox_types (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    color VARCHAR(255) NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at DATETIME NULL,
+    deleted_by INT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (deleted_by) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS cf_submissions (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    user_id INT NULL,
+    ip VARCHAR(255) NOT NULL,
+    user_agent VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    comments TEXT NULL,
+    comments_at DATETIME NULL,
+    comments_by INT NULL,
+
+    cf_inbox_type_id INT NULL,
+    contact_form_id INT NULL,
+    cf_label_id INT NULL,
+    read_at DATETIME NULL,
+    deleted_at DATETIME NULL,
+    starred_at DATETIME NULL,
+    archived_at DATETIME NULL,
+    spam_score INT NULL,
+    spam_reported_at DATETIME NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (comments_by) REFERENCES users(id),
+    FOREIGN KEY (cf_inbox_type_id) REFERENCES cf_inbox_types(id),
+    FOREIGN KEY (contact_form_id) REFERENCES contact_forms(id),
+    FOREIGN KEY (cf_label_id) REFERENCES cf_labels(id)
+);
+
+-- Seeding the Database
+
+INSERT INTO users(
+    name,
+    email,
+    password
+) VALUES (
+    'Admin',
+    'admin@mail.com',
+    'password'
+);
+
+INSERT INTO cf_inbox_types( name )
+VALUES
+    ('default'),
+    ('marketing'),
+    ('support'),
+    ('sales'),
+    ('billing'),
+    ('other');
